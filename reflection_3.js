f94dc409935e6c43217650ceff4249d5b321ee24
Zwe Zeya <No public email provided>
2023-08-30 17:31:02 +0800

    Changes made from Source Academy

---
+++ reflection_3.js
@@ -0,0 +1,27 @@
+function sum(term, a, next, b) { 
+    return a > b
+            ? 0
+            : term(a) + sum(term, next(a), next, b);
+}
+
+const my_sum = (n) => n === 1 ? 2 : (n + 1) * n + my_sum(n - 1);
+
+const my_sum2 = (n) => sum(x => x * (x + 1), 1, x => x + 1, n);
+
+const sum_iter = (term, a, next, b) => iter(0, term, a, next, b);
+
+const iter = (res, term, a, next, b) => a > b 
+        ? res 
+        : iter(res + term(a), term, next(a), next, b);
+        
+const my_sum3 = (n) => sum_iter(x => x * (x + 1), 1, x => x + 1, n);
+
+
+
+
+
+
+display(my_sum(7));
+display(my_sum2(7));
+display(my_sum3(7));
+0;
